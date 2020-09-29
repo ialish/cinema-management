f8efd3b1d2d5c2e9d6b867a4add5b5dc72296d1e
Eyal <eyal.shaar@gmail.com>
2020-09-29 16:04:20 +0300

    cinema-api

--- server/cinema-api/config/db.js
+++ server/cinema-api/config/db.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 const connectDB = () => {
-  const uri = 'mongodb://localhost:27017/users';
+	const uri = 'mongodb://localhost:27017/users';
   mongoose.connect(uri, {
     useNewUrlParser: true,
     useUnifiedTopology: true
