e00637c6f8bd2ed378a6bf99cae6f8d747cddcc1
Eyal <eyal.shaar@gmail.com>
2020-09-29 20:45:05 +0300

    cinema-api

--- server/cinema-api/index.js
+++ server/cinema-api/index.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const session = require('express-session');
 const connectDB = require('./configs/db');
 
 const app = express();
@@ -6,7 +7,14 @@ const port = process.env.PORT || 8080;
 
 connectDB();
 
+app.use(session({
+	secret: 'secret',
+	maxAge: 5 * 60000,
+  resave: true,
+  saveUninitialized: true
+}));
 app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 
 app.listen(port, () => {
 	console.log(`app listening at http://localhost:${port}`);
--- server/subscriptions-api/index.js
+++ server/subscriptions-api/index.js
@@ -7,7 +7,6 @@ const port = process.env.PORT || 8000;
 
 connectDB();
 
-app.use(express.json());
 app.use('/', indexRouter);
 
 app.listen(port, () => {
