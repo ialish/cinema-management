dfbffe46e74919790893578380c67d5c8899cd0e
Eyal <eyal.shaar@gmail.com>
2020-09-30 15:31:04 +0300

    index router

--- server/subscriptions-api/routes/index.js
+++ server/subscriptions-api/routes/index.js
@@ -3,6 +3,7 @@ const router = express.Router();
 
 const rootController = require('../controllers/root');
 
+// populating the members and movies collections of the subscriptions db
 router.get('/', rootController.add);
 
 module.exports = router;
